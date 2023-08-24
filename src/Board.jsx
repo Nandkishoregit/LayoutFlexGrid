f16ac5438e04a50240e8c057d0ce9d90b87ce4ff
Nandkishoregit <107661833+Nandkishoregit@users.noreply.github.com>
2023-08-24 09:51:22 -0400

    making flex-direction work

--- src/Board.jsx
+++ src/Board.jsx
@@ -1,17 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const Board = ({ selectedProperties }) => {
+const Board = ({
+  selectedProperties,
+  numSubContainers,
+  selectedFlexDirection, // Receive the selected flex direction
+  onAddSubContainer,
+  onRemoveSubContainer,
+}) => {
+  // Calculate the height for each sub-container
+  const totalAvailableHeight = 400; // Total height of the first container
+  const minHeight = 60; // Minimum height for each sub-container
+
+  useEffect(() => {
+    console.log("selectedFlexDirection", selectedFlexDirection);
+  }, [selectedFlexDirection]);
+  // Calculate dynamic height for sub-containers
+  const dynamicSubContainerHeight =
+    numSubContainers > 0
+      ? `${Math.min(totalAvailableHeight / numSubContainers, minHeight)}px`
+      : "100%";
+
   return (
     <div
       style={{
-        flex: 2,
-        border: "1px solid grey",
-        margin: "10px",
-        height: "400px",
+        display: "flex",
+        border: "1px solid red",
+        flexDirection: selectedFlexDirection, // Apply selected flex direction
+        width: "400px",
+        height: "400px", // Ensure sub-containers take full height
       }}
     >
-      <p>This is the first container (75% width)</p>
-      {/* Access and render selected properties */}
+      {[...Array(numSubContainers)].map((_, index) => (
+        <div
+          key={index}
+          style={{
+            border: "1px solid grey",
+            margin: "5px",
+            height: dynamicSubContainerHeight, // Set dynamic height
+            backgroundColor: "#f0f0f0",
+          }}
+        >
+          Sub Container {index + 1}
+        </div>
+      ))}
+      {/* Access and render selected properties
       {Object.keys(selectedProperties).length > 0 && (
         <div style={{ marginTop: "10px" }}>
           <p>Selected Properties:</p>
@@ -23,7 +55,7 @@ const Board = ({ selectedProperties }) => {
             ))}
           </ul>
         </div>
-      )}
+      )} */}
     </div>
   );
 };
--- src/BoardController.jsx
+++ src/BoardController.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const BoardController = ({
   properties,
+  selectedProperties,
   handleSelectChange,
-  selectedProperty,
+  selectedFlexDirection,
+  handleFlexDirectionChange,
 }) => {
   return (
     <div
@@ -14,16 +16,17 @@ const BoardController = ({
         height: "400px",
       }}
     >
-      <p>This is the second container (25% width)</p>
       <div style={{ marginTop: "20px" }}>
         {properties.map((property) => (
           <div key={property.label}>
             <label htmlFor={property.label}>{property.label}:</label>
             <select
               id={property.label}
-              onChange={(event) =>
-                handleSelectChange(property.label, event.target.value)
-              }
+              value={selectedProperties[property.label]}
+              onChange={(event) => {
+                console.log(event.target.value);
+                handleSelectChange(property.label, event.target.value);
+              }}
             >
               <option value="">Select</option>
               {property.values.map((value) => (
@@ -34,12 +37,33 @@ const BoardController = ({
             </select>
           </div>
         ))}
-        {selectedProperty && (
+        {Object.keys(selectedProperties).length > 0 && (
           <div style={{ marginTop: "10px" }}>
-            <p>Selected Property: {selectedProperty.property}</p>
-            <p>Selected Value: {selectedProperty.value}</p>
+            <p>Selected Properties:</p>
+            <ul>
+              {Object.entries(selectedProperties).map(([property, value]) => (
+                <li key={property}>
+                  {property}: {value}
+                </li>
+              ))}
+            </ul>
           </div>
         )}
+
+        {/* Select flex-direction */}
+        <div>
+          <label htmlFor="flex-direction">flex-direction:</label>
+          <select
+            id="flex-direction"
+            value={selectedFlexDirection}
+            onChange={(event) => handleFlexDirectionChange(event.target.value)}
+          >
+            <option value="row">row</option>
+            <option value="row-reverse">row-reverse</option>
+            <option value="column">column</option>
+            <option value="column-reverse">column-reverse</option>
+          </select>
+        </div>
       </div>
     </div>
   );
